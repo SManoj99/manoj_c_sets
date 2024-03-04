ad5f8cefd1074b652d2061beb3a8ef80edde4f25
SManoj99 <manojs.bsc23@rvu.edu.in>
2024-03-04 15:02:41 +0530

    text

--- set01/problem10.c
+++ set01/problem10.c
@@ -1,44 +1,41 @@
-#include <stdio.h>
+#include<stdio.h>
+#include<string.h>
 void input_two_strings(char *string1, char *string2);
 int stringcompare(char *string1, char *string2);
 void output(char *string1, char *string2, int result);
-
 int main()
 {
+    char *string1,*string2;
     int result;
-    char *string1[100],*string2[100];
-    input_two_strings(&string1,&string2);
-     result=stringcompare(&string1,&string2);
-    output(&string1,&string2,result);
+    input_two_strings(string1,string2);
+    result=stringcompare(string1,string2);
+    output(string1,string2,result);
     return 0;
 }
 void input_two_strings(char *string1, char *string2)
 {
-    printf("enter the string1 value:\n");
-    scanf("%s",string1);
-    printf("enter the string2 value:\n");
-    scanf("%s",string2);
+    printf("Enter the first string\n");
+    scanf("%s",&string1);
+    printf("Enter the second string\n");
+    scanf("%s",&string2);
 }
 int stringcompare(char *string1, char *string2)
-{ 
-    int i;
-    for (i=0;string1[i]!='\0' && string2[i]!='\0';i++)
+{
+    int i,result;
+    for(i=0;string1[i] || string2[i];i++)
     {
-        if(string1[i] != string2[i])
+        if(string1[i]>string2[i])
         {
-            return 1;
+            result=1;
+        }
+        else
+        {
+            result=2;
         }
     }
+    return result;
 }
 void output(char *string1, char *string2, int result)
 {
-    if(result==1)
-    {
-        printf("%s is not equal to %s",string1,string2);
-    }
-    else
-    {
-        printf("%s is equal to %s",string1,string2);
-    }
-    
+    printf("This %s string is latger than %s string and the result is %d\n",string1,string2,result);
 }
