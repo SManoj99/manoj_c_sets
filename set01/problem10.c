db3b706a5d5ec5c22de9c9850d822b2cc3258c5d
SManoj99 <manojs.bsc23@rvu.edu.in>
2024-03-04 14:50:10 +0530

    text

--- set01/problem10.c
+++ set01/problem10.c
@@ -6,51 +6,39 @@ void output(char *string1, char *string2, int result);
 int main()
 {
     int result;
-    char string1[100],string2[100];
-    input_two_strings(string1,string2);
-    result=stringcompare(string1,string2);
-    output(string1,string2,result);
+    char *string1[100],*string2[100];
+    input_two_strings(&string1,&string2);
+     result=stringcompare(&string1,&string2);
+    output(&string1,&string2,result);
+    return 0;
 }
-void input_two_strings(char *string1, char *string2 )
+void input_two_strings(char *string1, char *string2)
 {
-    printf("Enter the string : \n");
+    printf("enter the string1 value:\n");
     scanf("%s",string1);
-    printf("Enter the string : \n");
+    printf("enter the string2 value:\n");
     scanf("%s",string2);
 }
 int stringcompare(char *string1, char *string2)
-{
-    int a=0;
-    for(int i=0;string1[i]!='\0' || string2!='\0';i++)
+{ 
+    int i;
+    for (i=0;string1[i]!='\0' && string2[i]!='\0';i++)
     {
-        if(string1[i]>string2[i])
+        if(string1[i] != string2[i])
         {
-            a++;
-            return 0;
-        }
-        else if(string1[i]<string2[i])
-        {
-            a++;
             return 1;
         }
-    }
-    if(a==0)
-    {
-        return 2;
     }
 }
 void output(char *string1, char *string2, int result)
 {
-    if(result==0)
+    if(result==1)
     {
-        printf("%s is greater",string1);
-    }
-    else if(result==1)
-    {
-        printf("%s is greater",string2);
+        printf("%s is not equal to %s",string1,string2);
     }
     else
     {
-        printf("Both string are same");
+        printf("%s is equal to %s",string1,string2);
     }
+    
 }
